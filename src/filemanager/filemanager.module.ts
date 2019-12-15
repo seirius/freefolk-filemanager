3f1babbb926ce789cf82ece2f0b38ede8d8af015
Andriy <andsei91@gmail.com>
2019-12-15 16:11:56 +0100

    upd

--- src/filemanager/filemanager.module.ts
+++ src/filemanager/filemanager.module.ts
@@ -1,4 +1,4 @@
-import { Module, LoggerService } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { FileManagerService } from "./filemanager.service";
 import { FileManagerController } from "./filemanager.controller";
 
